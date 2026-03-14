1889056c0442da6d320b3fb025536e3877fb20a7
Azhar <azhar@pragtex.com>
2026-03-14 17:21:56 +0530

    chore: remove icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import Wrapper from "./UI/Wrapper";
-import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 import { SiHashnode } from "react-icons/si";
 const Footer = () => {
   return (
@@ -8,15 +8,12 @@ const Footer = () => {
       id="footer"
     >
       <div>
-        <div className="grid grid-cols-4 w-52 mx-auto place-items-center mb-5 text-cyan-950 text-2xl">
+        <div className="grid grid-cols-3 w-52 mx-auto place-items-center mb-5 text-cyan-950 text-2xl">
           <a href="https://github.com/axhar-ibraheem">
             <BsGithub className="text-slate-50 hover:text-amber-500" />
           </a>
           <a href="https://www.linkedin.com/in/axhar/">
             <BsLinkedin className="text-slate-50 hover:text-amber-500" />
-          </a>
-          <a href="https://twitter.com/axhar_0">
-            <BsTwitter className="text-slate-50 hover:text-amber-500" />
           </a>
           <a href="https://hashnode.com/@axhar">
             <SiHashnode className="text-slate-50 hover:text-amber-500" />
